bf61bd621f4a1e4032b75d4ec93c787f8f0bb9e9
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-07 12:15:00 -0500

    added starting files

---
+++ script/cook.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var recipes = [];
+
+$("#search-recipe-btn").on("click", function (event){
+    event.preventDefault();
+    var recipeSearchInput = "chicken"; //$(this).text();
+    console.log(recipeSearchInput);
+
+    var queryURL = "https://food2fork.com/api/search?q=" + recipeSearchInput + "&key=7cd47955dffd41deaec51a869580847a";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var searchResult = JSON.parse(response);
+        console.log(searchResult);
+        
+        // for(i=0; i<searchResult.recipes.lenght; i++){
+        //     $("#results-list").append('<li class="list-group-item" data-link=${searchResult.recipes[i].source.url}></li>')
+        //     console.log(searchResult.recipes[i].title)
+        // }
+        for(i=0; i<searchResult.recipes.lenght; i++){
+            $("#results-list").append(
+            '<div class="recipe-container"><a href=${searchResult.recipes[i].source_url}>${searchResult.recipes[i].title}</a><br/><img src=${searchResult.recipes[i].image_url}/>');
+        }
+
+});
+
+
+
+});
+});
