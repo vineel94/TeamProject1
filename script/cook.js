e0f25904a3fcc9b1492f88ca760f421460d85911
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-08 11:58:36 -0500

    fixed cors proxy issue for cook.js

--- script/cook.js
+++ script/cook.js
@@ -5,33 +5,35 @@ $(document).ready(function () {
         event.preventDefault();
         var recipeSearchInput = $("#dish-input").val();
 
-        if($("#ingredient-input").val())
-            recipeSearchInput += "," + $("#ingredient-input").val();
+        if ($("#ingredient-input").val())
+            recipeSearchInput += "%20" + $("#ingredient-input").val();
 
-        if($("#cuisine-input").val())
-            recipeSearchInput += "," + $("#cuisine-input").val();
+        if ($("#cuisine-input").val())
+            recipeSearchInput += "%20" + $("#cuisine-input").val();
 
         console.log(recipeSearchInput);
 
-        // var queryURL = "http://cors-proxy.htmldriven.com/?url=http://food2fork.com/api/search?key=7cd47955dffd41deaec51a869580847a&q=" + recipeSearchInput;
+        //var queryURL = "http://cors-proxy.htmldriven.com/?url=http://food2fork.com/api/search?key=7cd47955dffd41deaec51a869580847a&q=" + recipeSearchInput;
 
         var queryURL = "https://food2fork.com/api/search?key=7cd47955dffd41deaec51a869580847a&q=" + recipeSearchInput;
 
+        ///////////////////////////////////////////////////////
+        // heroku workaround for Cors
+        // Note: queryURL will be defined by the student.
+        var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        var targetUrl = queryURL;
 
-
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-            
-        }).then(function (response) {
-            
-            console.log(queryURL);
+        $.get(proxyUrl + targetUrl, function (response) {
             console.log(response);
-
-            // var searchResult = JSON.stringify(response);
-            // console.log(searchResult);
-
         });
+
+        ////////////////////////////////////////////////////////
+
+    
+        // var searchResult = JSON.stringify(response);
+        // console.log(searchResult);
+
+        // });
 
     });
 });
